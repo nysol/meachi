fe4ca4c57e6f5fe3cfb90f0770cea9c19e47d468
nain <nain0606@gmial.com>
2017-12-01 12:21:07 +0900

    wait修正

--- ext/meachi/meachi.c
+++ ext/meachi/meachi.c
@@ -2,20 +2,14 @@
 #include <mpi.h>
 
 
-void meachi(){
-	printf("Hello, world!\n");
-}
-
 int getIdleRank(MPI_Request *requests, int rank_size){
 	int i;
-
-	for(i=1;i<rank_size;i++){
+	for(i=0;i<rank_size;i++){
 		int complete;
 
 		MPI_Test(requests+i, &complete, MPI_STATUS_IGNORE);
-		if(complete){
-			return i;
-		}
+	
+		if(complete){ return i; }
 	}
 	return -1;
 }
@@ -32,6 +26,7 @@ VALUE wrap_meachi(VALUE self,VALUE val){
 	MPI_Status send_status;
 	MPI_Request req;
 	MPI_Request *requests;
+	MPI_Status *st;
 
 	int array_size=RARRAY_LEN(self);
 
@@ -41,34 +36,42 @@ VALUE wrap_meachi(VALUE self,VALUE val){
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &rank_size);
 
+	int wkSize = array_size > rank_size-1 ? rank_size-1 : array_size
+
 	if(my_rank==0){
 
-		requests=calloc(rank_size,sizeof(MPI_Request));
+		requests=calloc(wkSize,sizeof(MPI_Request));
+		st=calloc(wkSize,sizeof(MPI_Status));
 
 		index=0; // current element of ARRAY
-		for(i=1; i<rank_size; i++){ // worker rank starts from 1
-			if(index>=array_size) break;
-			MPI_Isend(&index,1,MPI_INT,i,0,MPI_COMM_WORLD,&req);
+		for(i=0; i<wkSize; i++){ // worker rank starts from 1
+
+			MPI_Isend(&index,1,MPI_INT,i+1,0,MPI_COMM_WORLD,&req);
 			MPI_Wait(&req,&send_status);
-			MPI_Irecv(&retv, 1,MPI_INT,i,0,MPI_COMM_WORLD,requests+i);
+			MPI_Irecv(&retv, 1,MPI_INT,i+1,0,MPI_COMM_WORLD,requests+i);
 
 			index++;
 		}
 
 		while(index<array_size){
-			idleRank=getIdleRank(requests,rank_size);
+			idleRank=getIdleRank(requests,wkSize);
 			if(idleRank<0) continue; // no idle rank found
-			MPI_Isend(&index,1,MPI_INT,idleRank,0,MPI_COMM_WORLD,&req);
+			MPI_Isend(&index,1,MPI_INT,idleRank+1,0,MPI_COMM_WORLD,&req);
 			MPI_Wait(&req,&send_status);
-			MPI_Irecv(&retv, 1,MPI_INT,idleRank,0,MPI_COMM_WORLD,requests+idleRank);
+			MPI_Irecv(&retv, 1,MPI_INT,idleRank+1,0,MPI_COMM_WORLD,requests+idleRank);
 			index+=1;
 		}
+
+		MPI_Waitall(wkSize, requests, st);
 
 		// send a end message to all workers
 		for(i=1; i<rank_size; i++){
 			index=-1;
 			MPI_Send(&index,1,MPI_INT,i,0,MPI_COMM_WORLD);
 		}
+		free(requests);
+		free(st);
+
 	}else{
 		while(1){
 			retv=0;
